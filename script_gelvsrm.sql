c343085eef438cfb3af04b7c5db126882ac2f6f1
eduardouio <accxell_ax3c@hotmail.es>
2013-10-01 11:03:50 -0500

    galo

--- script_gelvsrm.sql
+++ script_gelvsrm.sql
@@ -43,11 +43,11 @@ create database gelvsrm_polaris;
 	create table tecnico(
 		id_tecnico varchar(10) NOT NULL,
 		nombres varchar(200) NOT NULL,
-		telefono varchar(15),
-		celular  varchar(15),
-		email  varchar(100),
+		telefono varchar(15) DEFAULT NULL,
+		celular  varchar(15)DEFAULT NULL,
+		email  varchar(100)DEFAULT NULL,
 		registro timestamp not null default current_timestamp 
-		on update current_timestamp,
+		on update current_timestamp
 		primary key(id_tecnico)
 		)engine=innodb
 		COMMENT 'Entidad encargada de registrar los tecnicos';
@@ -60,9 +60,9 @@ create database gelvsrm_polaris;
 		nombre varchar(200) NOT NULL,
 		direccion varchar(600) NOT NULL,
 		telefono varchar(15) NOT NULL,
-		fax varchar(15),
-		fecha datetime,
-		mail varchar(100),
+		fax varchar(15)DEFAULT NULL,
+		fecha datetime DEFAULT NULL,
+		mail varchar(100)DEFAULT NULL,,
 		registro timestamp not null default current_timestamp 
 		on update current_timestamp,
 		primary key(id_cliente),
@@ -81,12 +81,12 @@ create database gelvsrm_polaris;
 		id_vehiculo char(16) NOT NULL,
 		id_cliente char(13) NOT NULL,
 		id_contacto smallint unsigned NOT NULL,
-		modelo varchar(45),
-		nro_motor varchar(25),
-		ingreso datetime, 
-		encargado varchar(50),
-		ubicacion varchar(100),
-		notas text,
+		modelo varchar(45)DEFAULT NULL,
+		nro_motor varchar(25)DEFAULT NULL,
+		ingreso datetime DEFAULT NULL, 
+		encargado varchar(50)DEFAULT NULL,
+		ubicacion varchar(100)DEFAULT NULL,
+		notas text DEFAULT NULL,
 		registro timestamp not null default current_timestamp 
 		on update current_timestamp,
 		primary key(id_vehiculo),
@@ -105,14 +105,14 @@ create database gelvsrm_polaris;
 	-- Estructura de la entidad inspección
 	-- -------------------------------------------------------------------
 	create table inspeccion(
-		id_inspeccion smallint unsigned NOT NULL,
+		id_inspeccion smallint unsigned NOT NULL AUTO_INCREMENT,
 		id_vehiculo varchar(16) NOT NULL,
 		id_tecnico varchar(10) NOT NULL,
 		id_contacto smallint unsigned NOT NULL,
 		fecha datetime NOT NULL,
-		ubicacion varchar(100),
+		ubicacion varchar(100)DEFAULT NULL,
 		detalle text,
-		contacto varchar(45),
+		contacto varchar(45)DEFAULT NULL,
 		registro timestamp not null default current_timestamp 
 		on update current_timestamp,
 		primary key(id_inspeccion),
@@ -129,17 +129,18 @@ create database gelvsrm_polaris;
       	REFERENCES contacto(id_contacto)
       	ON DELETE RESTRICT ON UPDATE CASCADE
 		)engine=innodb
+	AUTO_INCREMENT = 1
 		COMMENT 'De momento es solo un formulario de inspeccion';
 	-- -------------------------------------------------------------------
 	-- Estructura de la reparación
 	-- -------------------------------------------------------------------
 	create table reparacion(
-		id_reparacion smallint unsigned NOT NULL,
+		id_reparacion smallint unsigned NOT NULL AUTO_INCREMENT,
 		id_vehiculo char(16) NOT NULL,
 		id_tecnico varchar(10) NOT NULL,
 		fecha_entrada datetime NOT NULL,
-		fecha_salida datetime,
-		detalles text,
+		fecha_salida datetime DEFAULT NULL,
+		detalles text DEFAULT NULL,
 		registro timestamp not null default current_timestamp 
 		on update current_timestamp,
 		primary key(id_reparacion),
@@ -152,6 +153,7 @@ create database gelvsrm_polaris;
       	REFERENCES tecnico(id_tecnico)
       	ON DELETE RESTRICT ON UPDATE CASCADE
 		)engine=innodb
+	AUTO_INCREMENT = 1
 		COMMENT 'Entidad encargada de registrar las reparaciones realizadas a los vehiculos polaris,
 		cada reparacion al vehiculo se registra en reparacion_detalle';
 	-- -------------------------------------------------------------------
@@ -160,9 +162,9 @@ create database gelvsrm_polaris;
 	create table reparacion_detalle(
 		id_reparacion_detalle smallint unsigned NOT NULL,
 		id_reparacion smallint unsigned NOT NULL,
-		item varchar(200),
+		item varchar(200)DEFAULT NULL,
 		estado varchar(25) COMMENT 'bueno, exelente,reparar,dañado,necesita cambio',
-		observaciones varchar(600),
+		observaciones varchar(600)DEFAULT NULL,
 		registro timestamp not null default current_timestamp 
 		on update current_timestamp,
 		primary key(id_reparacion_detalle),
@@ -177,14 +179,14 @@ create database gelvsrm_polaris;
 	-- Estructura de la entidad mantenimiento
 	-- -------------------------------------------------------------------
 	create table mantenimiento(
-		id_manteminiento smallint unsigned NOT NULL,
+		id_manteminiento smallint unsigned NOT NULL AUTO_INCREMENT,
 		id_vehiculo char(16) NOT NULL,
 		id_tecnico varchar(10) NOT NULL,
 		periodo smallint unsigned NOT NULL,
 		ubicacion varchar(100),
 		fecha datetime NOT NULL,
 		kilometros varchar(10) NOT NULL,
-		observacion text,
+		observacion text DEFAULT NULL,
 		registro timestamp not null default current_timestamp 
 		on update current_timestamp,
 		primary key(id_manteminiento),
@@ -197,6 +199,7 @@ create database gelvsrm_polaris;
       	REFERENCES tecnico(id_tecnico)
       	ON DELETE RESTRICT ON UPDATE CASCADE
 		)engine=innodb
+	AUTO_INCREMENT = 1
 		COMMENT 'Registra los mantenimientos realizados aun vehiculo, los insumos usados para el mantenimiento
 				se registran en mantenimiento_detalle';
 	-- -------------------------------------------------------------------
@@ -205,7 +208,7 @@ create database gelvsrm_polaris;
 	create table mantenimiento_detalle(
 		id_manteminiento_detalle int unsigned NOT NULL,
 		id_manteminiento smallint unsigned NOT NULL,
-		item varchar(200),
+		item varchar(200)DEFAULT NULL,
 		estado varchar(25) COMMENT 'cambio,reparacio,correccio',
 		observaciones varchar(600),
 		registro timestamp not null default current_timestamp 
@@ -221,18 +224,19 @@ create database gelvsrm_polaris;
 	-- Estructura de la entidad inventario
 	-- -------------------------------------------------------------------
 	create  table inventario(
-		id_inventario smallint unsigned  NOT NULL,
+		id_inventario smallint unsigned  NOT NULL AUTO_INCREMENT,
 		fecha datetime NOT NULL,
 		nombre varchar(100) NOT NULL,
-		descpricion varchar(200),
-		unidad varchar(25),
+		descpricion varchar(200)DEFAULT NULL,
+		unidad varchar(25)DEFAULT NULL,
 		stok_min smallint unsigned NOT NULL COMMENT "12 filtros,12 aceite, 4 refirgerante 1 liquido de frenos",
-		marca varchar(45),
+		marca varchar(45)DEFAULT NULL,
 		ubicacion varchar(100) NOT NULL,
 		registro timestamp not null default current_timestamp 
 		on update current_timestamp,
 		primary key(id_inventario)
 		)engine=innodb 
+	AUTO_INCREMENT = 1
 		COMMENT 'Entidad que lleva un detalle de los productos en bodega
 		Todos los materiales están en litros y unidades, se setablece un standar para los 
 		condigos del inventario, cada producto ingresado al inventario empieza con el mail
@@ -241,7 +245,7 @@ create database gelvsrm_polaris;
 	-- Estructura de la entidad entradas de inventario
 	-- -------------------------------------------------------------------
 	create table inventario_entrada(
-		id_inventario_entrada smallint unsigned NOT NULL,
+		id_inventario_entrada smallint unsigned NOT NULL, 
 		id_inventario smallint unsigned  NOT NULL,
 		id_proveedor varchar(13) NOT NULL ,
 		nro_factura varchar(12) NOT NULL ,
@@ -272,8 +276,8 @@ create table inventario_salida(
 		id_manteminiento_detalle int unsigned NOT NULL,
 		id_reparacion_detalle smallint unsigned NOT NULL,
 		cantidad smallint unsigned NOT NULL,
-		fecha datetime,
-		notas text,
+		fecha datetime DEFAULT NULL,
+		notas text DEFAULT NULL,
 		registro timestamp not null default current_timestamp 
 		on update current_timestamp,
 		primary key(id_inventario_salida),
@@ -299,13 +303,13 @@ create table inventario_salida(
 	-- Estructura de la entidad factura
 	-- -------------------------------------------------------------------
 	create table factura(
-		id_factura smallint unsigned NOT NULL,
+		id_factura smallint unsigned NOT NULL AUTO_INCREMENT,
 		id_cliente char(13) NOT NULL,
 		fecha datetime NOT NULL,
 		fecha_envio datetime NOT NULL,
 		estado boolean NOT NULL,
-		archivo varchar(200),
-		notas text,
+		archivo varchar(200)DEFAULT NULL,
+		notas text DEFAULT NULL,
 		registro timestamp not null default current_timestamp 
 		on update current_timestamp,
 		primary key(id_factura),
@@ -314,6 +318,7 @@ create table inventario_salida(
       	REFERENCES cliente(id_cliente)
       	ON DELETE RESTRICT ON UPDATE CASCADE
 		)engine=innodb
+	AUTO_INCREMENT = 1
 		COMMENT 'Entidad encargada de registrar las facturación, se implementa una ruta
 		al archivo del escaneado de la factura';	
 	-- -------------------------------------------------------------------
