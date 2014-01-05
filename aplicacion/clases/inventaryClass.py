fd197d38d6aef40d0baf36c09d5a89aa0b4de317
eduardouio <eduardouio7@gmail.com>
2014-01-04 23:09:57 -0500

    se edita la clase inventarion catalog

--- aplicacion/clases/inventaryClass.py
+++ aplicacion/clases/inventaryClass.py
@@ -26,7 +26,7 @@
 import sys
 sys.path.append('..')
 from modelo.Modelo import DB
-from PyQt4.QtCore import QDateTime, QDate, QTime
+from PyQt4.QtCore import QDateTime, QDate, QTime, qDebug
 
 class Inventary(object):
 	"""descripcion de la clase Inventario"""
@@ -34,63 +34,86 @@ class Inventary(object):
 				unidad='',stok_min='',marca='',ubicacion='', notas='', registro=''):
 		super(Inventario, self).__init__()
 		self.id_inventario = id_inventario
-		self.fecha = fecha
+		self.fecha = QDate().currentDate()
 		self.nombre = nombre
 		self.descripcion = descripcion
 		self.unidad = unidad
 		self.stok_min = stok_min
 		self.marca = marca
 		self.ubicacion = ubicacion
-		self.notas = notas
-		#se pone la hora actual en el nuevo objeto
+		self.notas = notas		
 		self.registro = QDateTime().currentDateTime()
+		qDebug('[Debug] se inicia la clase inventary')
+
 
 class inventaryCatalog(object):
 	"""acciones sobre Inventary"""
+
 	def __init__(self, arg):
 		super(inventaryCatalog, self).__init__()
 		self.table = 'inventario'
 		self.MyDb = DB()
+		qDebug('[Debug] se inicia la clase inventaryCatalog')
+
 
 	def getInventary(self,inventary=''):
 		'''Obtiene un listado de inventario o inventario
 		@param inventary
 		@return (obj) | list(obj)'''
-		if inventary:
-			myinventary = Inventary()
-			condition = ' id_inventario = ' + str(inventary)
-			result = self.MyDb.selectQuery(self.table,'',condition)
-			while result.next():
-				myinventary.id_inventario = str(result.value(0))
-				myinventary.fecha = str(result.value(1))
-				myinventary.nombre = str(result.value(2))
-				myinventary.descripcion = str(result.value(3))
-				myinventary.unidad = str(result.value(4))
-				myinventary.stok_min = str(result.value(5))
-				myinventary.marca = str(result.value(6))
-				myinventary.ubicacion = str(result.value(7))
-				myinventary.notas = str(result.value(8))
-				myinventary.registro = str(result.value(9))
-
-			return myinventary
+		condition = {' id_inventario = ' : str(inventary)}
+		result = self.MyDb.selectQuery(self.table,'',condition)
+		qDebug('[Debug] la consulta retorno %s objetos'% str(result.size()))
+		if result.next():
+			return self.__setObj(result)
 		else:
-			inventary = []
-			result = self.MyDb.selectQuery(self.table)
-			while result.next():
-				myinventary = DB()
-				myinventary.id_inventario = str(result.value(0))
-				myinventary.fecha = str(result.value(1))
-				myinventary.nombre = str(result.value(2))
-				myinventary.descripcion = str(result.value(3))
-				myinventary.unidad = str(result.value(4))
-				myinventary.stok_min = str(result.value(5))
-				myinventary.marca = str(result.value(6))
-				myinventary.ubicacion = str(result.value(7))
-				myinventary.notas = str(result.value(8))
-				myinventary.registro = str(result.value(9))
-				inventary.append(myinventary)
+			qDebug('[Debug] no se puede encontrar el item de inventario')
+			return False
+	
 
-			return inventary
+	def listInventary(self):
+		'''Lista todos los items del invemtario
+		@return lst(inventary)'''	
+		inventary = []
+		result = self.MyDb.selectQuery(self.table)
+		while result.next():			
+			inventary.append(self.__setObj(result))
+
+		return inventary
+
+
+	def firstInventary(self):
+		'''retorna el primer item del inventary de la lista
+		@return (obj) Inventary'''		
+		result = self.MyDB.selectQuery(self.table)
+		qDebug('[Debug] Se toma el primer Inventario de la lista')
+		if result.first():
+			return self.__setObj(result)
+		else:
+			return False
+
+
+	def lastInventary(self):
+		'''retorna el ultimo Inventario de la Lista
+		@return (obj) Inventario'''
+		result = self.MyDB.selectQuery(self.table)
+		qDebug('[Debug] Se toma ultimo Inventario de la lista')
+		if result.last():
+			return self.__setObj(result)
+		else:
+			return False
+
+
+	def findInventary(self,condition):
+		'''Busca un Inventaryo
+		@param condition = {'id_tecnico like ' : '%4%'}
+		@return list(obj) tipo Inventary'''
+		Inventary = []
+		result = self.MyDB.selectQuery(self.table,'',condition)
+		while result.next():
+			Inventary.append(self.__setObj(result))
+
+		return Inventary
+
 
 	def createInventary(self,inventary):
 		'''Crea un inventario
@@ -104,21 +127,23 @@ class inventaryCatalog(object):
 			'stok_min' : str(inventary.stok_min),
 			'marca' : str(inventary.marca),
 			'ubicacion' : str(inventary.ubicacion),
-			'notas' : str(inventary.notas),
-			'registro' : str(inventary.registro)
+			'notas' : str(inventary.notas)			
 		}
 
 		result = self.MyDb.insertQuery(self.table,values)
 
 		if(result.numRowsAffected()>0):
+			qDebug('[Debug] Se crea un item de inventario en la tabla invemtario')
 			return str(result.lastInsertId())
 		else:
+			qDebug('[Debug] no se puede crear un item de inventario en la tabla invemtario')
 			return False
+
 
 	def updateInventary(self,oldInventary, inventary):
 		'''Actualiza un inventario
 		@param'''
-		condition = ' id_inventario =' + str(oldInventary.id_inventario)
+		condition = {' id_inventario =' : str(oldInventary.id_inventario)}
 		values = {
 			'fecha' : str(inventary.fecha),
 			'nombre' : str(inventary.nombre),
@@ -127,29 +152,67 @@ class inventaryCatalog(object):
 			'stok_min' : str(inventary.stok_min),
 			'marca' : str(inventary.marca),
 			'ubicacion' : str(inventary.ubicacion),
-			'notas' : str(inventary.notas),
-			'registro' : str(inventary.registro)
+			'notas' : str(inventary.notas)			
 		}
 
 		result = self.updateQuery(self.table,values)
 
 		if(result.numRowsAffected()>0):
+			qDebug('[Debug] Se Actualiza un item de inventario en la tabla invemtario')
 			return str(result.lastInsertId())
 		else:
+			qDebug('[Debug] problemas para Actualizar un item de inventario en la tabla invemtario')
 			return False
+
 
 	def deleteInventary(self,inventary):
 		'''Elimina un item del inventario
 		@param (obj)
 		@return (bool)'''
-		condition = ' id_inventario = ' + str(inventary.id_inventario())
+		condition = {' id_inventario = ' : str(inventary.id_inventario())}
 		result = self.MyDb.deleteQuery(self.table, condition)
 
 		if(result.numRowsAffected()>0):
+			qDebug('[Debug] Se Elimina un item de inventario en la tabla invemtario')
 			return str(result.lastInsertId())
 		else:
+			qDebug('[Debug] no se pude eliminar un item de inventario en la tabla invemtario')
 			return False
 
-	def findInventary(self,condition=''):
-		'''Busca un inventary'''
-		pass
+
+	def countInventary(self):
+		'''Cuenta los items de inentario registrados
+		@return (int)'''
+		qDebug('[Debug] se cuentan los registros de la tabla Inventario')
+		return len(self.listInventary())
+
+
+	def listColumns(self):
+		'''Retorna un listado de las columnas de la tabla inventary
+		@return (lstt)'''
+		colums = []
+		result = self.MyDB.listColumns(self.table)
+		while result.next():
+			qDebug('[Debug] se lista las columnas de la tabla Inventario')
+			colums.append(str(result.value(0)))
+
+		return colums
+
+
+	def __setObj(self, result):
+		'''Crea un objeto tipo inventary y lo retorna
+		@param (obj) result
+		@return (obj) inventary'''
+		myinventary = Inventary()
+		myinventary.id_inventario = str(result.value(0))
+		myinventary.fecha = str(result.value(1))
+		myinventary.nombre = str(result.value(2))
+		myinventary.descripcion = str(result.value(3))
+		myinventary.unidad = str(result.value(4))
+		myinventary.stok_min = str(result.value(5))
+		myinventary.marca = str(result.value(6))
+		myinventary.ubicacion = str(result.value(7))
+		myinventary.notas = str(result.value(8))
+		myinventary.registro = str(result.value(9))
+
+		return myinventary
